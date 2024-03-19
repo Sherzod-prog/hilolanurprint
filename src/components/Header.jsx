721474c787aca7ef7fa0f863029ff7e037776500
Sherzod-prog <sherzodnishonov@gmail.com>
2024-03-19 12:52:34 +0500

    finish6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,7 +93,7 @@ export default function Example({ theme, setTheme }) {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-blue-50"
                     >
                       {item.name}
                     </a>
